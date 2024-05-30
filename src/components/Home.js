84ecdbc4a90c64fc35693fd2872e32f244b7b631
Adams-Ghub <bamfoadamsf@gmail.com>
2024-05-30 09:44:20 +0000

    Resolve linter errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,22 @@ const Home = () => (
   <div className={styles.mainContainer}>
     <h1>Welcome to our page !</h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aut sed
-      magnam dolorem rerum nobis praesentium beatae nostrum iusto aspernatur amet
-      voluptatum quis corrupti, sunt vitae saepe incidunt accusantium delectus.
+      Welcome to Maths magician, where we bring together the precision of
+      numbers and the inspiration of words. Our user-friendly calculator is
+      designed to meet your everyday computational needs, from basic arithmetic
+      to more complex calculations, all with ease and efficiency. Whether you’re
+      managing your finances, solving homework problems, or planning a project,
+      our calculator is here to help you navigate the numbers with confidence.
     </p>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Soluta aut sed magnam dolorem rerum nobis praesentium beatae
-      nostrum iusto aspernatur amet voluptatum quis corrupti, sunt vitae
-      saepe incidunt accusantium delectus.
+      In addition to providing a powerful calculation tool, we believe in
+      enriching your experience with a touch of inspiration. As you work through
+      your calculations, take a moment to enjoy our collection of motivational
+      and thought-provoking quotes. These carefully selected words of wisdom are
+      here to uplift your spirits, spark your creativity, and offer a refreshing
+      pause in your busy day. We hope you find both the functionality and the
+      inspiration you seek as you explore our website. Welcome, and enjoy your
+      stay!
     </p>
   </div>
 );
